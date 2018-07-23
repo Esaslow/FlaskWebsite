ce270ed1e912ce1186e94bf2df8d3f8e1080e03a
Esaslow <Elsa7762@colorado.edu>
2018-07-23 10:57:13 -0700

    commiting

--- static/mpg.js
+++ static/mpg.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
-
   console.log('document is ready');
 
   $('#inference').click(async function(){
-
     console.log('button was clicked');
+
+    const cylinders = parseFloat($('#cylinders').val());
+    const horsepower = parseFloat($('#horsepower').val());
+    const weight = parseFloat($('#weight').val());
+    const data = {
+      cylinders,
+      horsepower,
+      weight
+    }
+    console.log(data)
   })
 })
